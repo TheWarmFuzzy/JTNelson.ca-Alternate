6ee0937ab718a54d9aff216f6f9ed3a9210f616c
Jeffrey Nelson <asgardolympian@gmail.com>
2015-05-14 14:19:45 -0400

    Added Sidebar and Header
    -Removed debug output from css

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<html>
+<!DOCTYPE html>
+<html lang="en-US">
 	<head>
-	
+		<meta charset="utf-8">
 		<!--
 		<script type="text/javascript" src=""></script>
 		
@@ -10,14 +11,31 @@
 	</head>
 	<body>
 		<header>
-		
+			<div id="inner_header">
+				<h1>
+					Jeffrey Nelson
+				</h1>
+				<nav>
+					<ul>
+						<a href =""><li>Home</li></a>
+						<a href =""><li>Portfolio</li></a>
+						<a href =""><li>Contact</li></a>
+					</ul>
+				</nav>
+			</div>
 		</header>
-		<nav>
 		
-		</nav>
 		
-		<aside>
+		<a href="#sidebar">
+			<aside class="hidden">
+				<svg id="arrow" width="50" height="50">
+					<path d="M0 0 L25 25 L0 50 L0 42 L17 25 L0 8 Z" />
+				</svg>
+			</aside>
+		</a>
+		<aside id="sidebar">
 			
+			<a href="#" class="mask"/>
 		</aside>
 	</body>
 </html>
